5ffdfa0aa8ce5a54abb718e0cf90521643e59ee7
Milton Pineda <miltonpineda@MBP17C02V416UHTD7.local>
2020-06-03 23:44:46 -0600

    using showntell

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar, FadeHeader } from "showntell";
 
 function App() {
+  const navigation = [
+    { title: "Inicio", type: "item", href: "#inicio" },
+    { title: "Servicios", type: "item", href: "#servicios" },
+    { title: "Quienes Somos", type: "item", href: "#nosotros" },
+    { title: "Transportes", type: "item", href: "#transportes" },
+    { title: "Galería", type: "item", href: "#galeria" },
+    { title: "Contáctenos", type: "item", href: "#contacto" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar
+        brand={"Carbus"}
+        navigation={navigation}
+        customBgColor="#2f3542"
+      ></Navbar>
+      <div className="section_container">
+        <section id="inicio">
+          <FadeHeader>Hello</FadeHeader>
+          <h1>Inicio</h1>
+          <p>Hello</p>
+        </section>
+
+        <section id="servicios">
+          <h1>Servicios</h1>
+          <p>Hello</p>
+        </section>
+        <section id="nosotros">
+          <h1>Nosotros</h1>
+          <p>Hello</p>
+        </section>
+        <section id="transportes">
+          <h1>Transportes</h1>
+          <p>Hello</p>
+        </section>
+        <section id="galeria">
+          <h1>Galeria</h1>
+          <p>Hello</p>
+        </section>
+        <section id="contacto">
+          <h1>Contacto</h1>
+          <p>Hello</p>
+        </section>
+      </div>
     </div>
   );
 }
